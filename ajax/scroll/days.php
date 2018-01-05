4d2621964aae165fbb068275880141d6bb7347b7
EugeneKovalov <eugene.kovalev@me.com>
2018-01-05 17:25:05 +0300

    ajax scroll - in progress

--- ajax/scroll/days.php
+++ ajax/scroll/days.php
@@ -5,6 +5,8 @@ error_reporting(1);
 
 $data = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['limit'])
+{
+    $output = json_encode(array_values($data));
+    echo "addElements($output);";
 }
--- ajax/scroll/scroll.js
+++ ajax/scroll/scroll.js
@@ -1,14 +1,36 @@
-let from = 0;
-let limit = 3;
+let limit = 5;
+let from = Math.ceil(document.documentElement.clientHeight / 300);
+let initial = Math.ceil(document.documentElement.clientHeight / 300) - 1;
 
-let initial = Math.ceil(document.documentElement.clientHeight / 300);
-
-for (var i = 0; i < initial; i++) {
-    let el = document.createElement('div');
-    el.setAttribute('class', 'container');
-    document.body.appendChild(el);
+function addElements(days) {
+    for (let i = from - initial, j = 0; i < from+limit; i++, j++) {
+        let div = document.createElement('div');
+        let span = document.createElement('span');
+        span.innerText = '' + i + ' : ' + days[j];
+        div.setAttribute('class', 'container');
+        div.appendChild(span);
+        document.body.appendChild(div);
+    }
+    from += initial + limit;
+    document.head.removeChild(document.getElementById('tmp'));
+    console.log(document.getElementsByClassName('container').length);
 }
 
-function loadData(start, from) {
-
+function update() {
+    let elem = document.createElement("script");
+    elem.setAttribute('id', 'tmp');
+    elem.src = "days.php?from=" + from + "&limit=" + limit;
+    document.head.appendChild(elem);
 }
+
+update();
+
+window.onscroll = function() {
+    let doc = document.documentElement;
+    let offset = doc.scrollTop + window.innerHeight;
+    let height = doc.offsetHeight;
+
+    if (offset === height) {
+        update();
+    }
+};
