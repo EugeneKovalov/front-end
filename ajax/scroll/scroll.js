ad90b2925c543cf0d35e505a71dd092a699ce313
EugeneKovalov <eugene.kovalev@me.com>
2018-01-09 09:57:34 +0300

    ajax scroll - fixes

--- ajax/scroll/scroll.js
+++ ajax/scroll/scroll.js
@@ -1,40 +1,42 @@
-let limit = Math.ceil(document.documentElement.clientHeight / 300);
-let from = 1;
+let initData = {
+    limit: Math.ceil(document.documentElement.clientHeight / 300),
+    from: 1,
+    document: document.documentElement,
+    isActive: false
+}
 
 function addElements(start, to, days) {
-    // console.log(start + ' st');
-    // console.log(to + ' to');
-    // console.log(days);
-    for (let i = 0; start < to; ++start, i++) {
-        let div = document.createElement('div');
-        let span = document.createElement('span');
+    if (!initData.isActive) {
+        initData.isActive = true;
+        for (let i = 0; start < to; ++start, i++) {
+            let div = document.createElement('div');
+            let span = document.createElement('span');
 
-        span.innerText = start + ' : ' + days[i];
-
-        div.setAttribute('class', 'container');
-        div.appendChild(span);
-        document.body.appendChild(div);
+            span.innerText = start + ' : ' + days[i];
+            div.setAttribute('class', 'container');
+            div.appendChild(span);
+            document.body.appendChild(div);
+        }
+        initData.from = start;
+        initData.isActive = false;
+        document.head.removeChild(document.getElementById('tmp'));
     }
-    from = start;
-
-    document.head.removeChild(document.getElementById('tmp'));
     document.getElementById('counter').innerText = '' + document.getElementsByClassName('container').length;
 }
 
 function update() {
-    let elem = document.createElement("script");
+    let elem = document.createElement('script');
     elem.setAttribute('id', 'tmp');
-    elem.src = "days.php?from=" + from + "&limit=" + limit;
+    elem.src = "days.php?from=" + initData.from + "&limit=" + initData.limit;
     document.head.appendChild(elem);
 }
 
 update();
-limit = 5;
+initData.limit = 5;
 
 window.onscroll = function() {
-    let doc = document.documentElement;
-    let offset = doc.scrollTop + window.innerHeight;
-    let height = doc.offsetHeight;
+    let offset = initData.document.scrollTop + window.innerHeight;
+    let height = initData.document.offsetHeight;
 
     if (offset === height) {
         update();
