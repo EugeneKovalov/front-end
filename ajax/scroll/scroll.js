b2a90c47c9697e0ab8d20118aaea854b93876f60
EugeneKovalov <eugene.kovalev@me.com>
2018-01-13 18:56:10 +0300

    ajax tic-tac-toe -in progress

--- ajax/scroll/scroll.js
+++ ajax/scroll/scroll.js
@@ -6,29 +6,29 @@ let initData = {
 }
 
 function addElements(start, to, days) {
-    if (!initData.isActive) {
-        initData.isActive = true;
-        for (let i = 0; start < to; ++start, i++) {
-            let div = document.createElement('div');
-            let span = document.createElement('span');
+    for (let i = 0; start < to; ++start, i++) {
+        let div = document.createElement('div');
+        let span = document.createElement('span');
 
-            span.innerText = start + ' : ' + days[i];
-            div.setAttribute('class', 'container');
-            div.appendChild(span);
-            document.body.appendChild(div);
-        }
-        initData.from = start;
-        initData.isActive = false;
-        document.head.removeChild(document.getElementById('tmp'));
+        span.innerText = start + ' : ' + days[i];
+        div.setAttribute('class', 'container');
+        div.appendChild(span);
+        document.body.appendChild(div);
     }
+    initData.from = start;
+    document.head.removeChild(document.getElementById('tmp'));
     document.getElementById('counter').innerText = '' + document.getElementsByClassName('container').length;
 }
 
 function update() {
-    let elem = document.createElement('script');
-    elem.setAttribute('id', 'tmp');
-    elem.src = "days.php?from=" + initData.from + "&limit=" + initData.limit;
-    document.head.appendChild(elem);
+    if (!initData.isActive) {
+        initData.isActive = true;
+        let elem = document.createElement('script');
+        elem.setAttribute('id', 'tmp');
+        elem.src = "days.php?from=" + initData.from + "&limit=" + initData.limit;
+        document.head.appendChild(elem);
+        initData.isActive = false;
+    }
 }
 
 update();
